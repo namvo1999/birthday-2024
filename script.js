3d3cecb35e7ef1b39b99b2335d03c87970743b3b
Nam <namptbt@gmail.com>
2024-04-21 15:33:14 +0700

    gogo

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
 let step = 0;
-const defaultContentTop = '25px';
-document.getElementById("wrapper").addEventListener("click", function() {
-    var content = document.getElementById("content");
-    var content2 = document.getElementById("content-2");
-    this.classList.add("clicked");
-    step += 1;
-    
-    if(step == 3){
-        content.classList.add("clicked");
-        content.style.top = '475px';
-        content2.style.top = '900px';
-    }
+let contentStep = 0;
+let numbContent = 3;
+const defaultContentTop = 25;
 
-    if(step == 2){
-        content2.classList.add("clicked");
-        content2.style.top = '475px';
-        content.style.top = defaultContentTop;
-    }
+let emotionalStep = 0;
+let emotionals = ["Buồn 😞", "vui 😊", "tìm người nói chuyện 🙄"]
+let currentTop = defaultContentTop;
+let emotionalText = document.getElementById("emotional");
 
-    if(step == 4) {
-        this.classList.remove("clicked");
-        content.classList.remove("clicked");
-        content2.classList.remove("clicked");
-        content2.style.top = defaultContentTop;
-        content.style.top = defaultContentTop;
+function resizeTo() {
+  step = 0;
+  contentStep = 0;
+  currentTop = defaultContentTop;
 
-        step = 0;
+  for (var i = numbContent; i >= 1; i--) {
+    var content = document.getElementById(`content-${i}`);
+    content.classList.remove("clicked");
+    content.style.top = (defaultContentTop - i) + 'px';
+    content.style.transform = "";
+  };
+}
+
+document.getElementById("wrapper").addEventListener("click", function () {
+  this.classList.add("clicked");
+  step += 1;
+
+  //   if (step == 3) {
+  //     content.classList.add("clicked");
+  //     content.style.top = "475px";
+  //     content2.style.top = "925px";
+  //     content.style.transform = "";
+  //   }
+
+  //   if (step == 2) {
+  //     content2.classList.add("clicked");
+  //     content2.style.top = "480px";
+  //     content.style.top = defaultContentTop;
+  //     content2.style.transform = "";
+  //   }
+
+  if (step > 1) {
+    contentStep += 1;
+    let top = contentStep * 270;
+    for (var i = numbContent; i >= 1; i--) {
+      var content = document.getElementById(`content-${i}`);
+      content.style.top = top + "px";
+      top -= 270;
+      if (top <= 0) {
+        top = defaultContentTop;
+      }
     }
+  } 
+
+  if (step == 4) {
+    this.classList.remove("clicked");
+    resizeTo();
+  }
 });
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
